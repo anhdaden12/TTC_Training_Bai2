335ed1cc078753f757b21512204a9f01c4f383c6
ThuyNT <thuynt@ttc-solutions.com.vn>
2021-01-18 23:44:57 +0700

    Initial Commit

---
+++ GCD_Bai_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD_Bai_2
+//
+//  Created by Apple on 1/18/21.
+//  Copyright © 2021 NgocNB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
